4066d550edff4a06ee570d185e30a4370b04ed26
jdbanuel <jamesdb1995@gmail.com>
2019-11-19 22:55:04 -0800

    Working on banner Building general layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container mx-auto">
+            <div className="flex justify-center bg-purple-500 h-40 shadow-2xl text-gray-100 font-semibold tracking-wider">
+                <div className=" flex max-w text-5xl items-center">
+                    Debt Repayment Calculator
+                </div>
+            </div>
+
+            <div className="flex justify-center">
+                <div className="flex-none">Left</div>
+                <div classname="flex-none">Right</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
