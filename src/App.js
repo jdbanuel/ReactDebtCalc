726e881c87db8c4dbbe82c1c09ce190d6e5afdfb
jdbanuel <jamesdb1995@gmail.com>
2019-11-21 21:27:44 -0800

    importing navbar into App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from '../src/components/Navbar.component.jsx';
 
 function App() {
     return (
         <div className="container mx-auto">
-            <div className="flex justify-center bg-purple-500 h-40 shadow-2xl text-gray-100 font-semibold tracking-wider">
-                <div className=" flex max-w text-5xl items-center">
-                    Debt Repayment Calculator
-                </div>
-            </div>
-
+            <Navbar />
             <div className="flex justify-center">
                 <div className="flex-none">Left</div>
                 <div classname="flex-none">Right</div>
