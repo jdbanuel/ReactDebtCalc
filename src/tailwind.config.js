225cb622151105de1c07b565a71fbc0a61fe1f17
jdbanuel <jamesdb1995@gmail.com>
2020-02-18 21:58:18 -0800

    Updating tailwind pixel sizes

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+        extend: {},
+        height: {
+            sm: '80px',
+            md: '160px',
+            lg: '300px',
+            xl: '480px',
+            xxl: '700px'
+        }
+    },
+    variants: {},
+    plugins: []
+};
