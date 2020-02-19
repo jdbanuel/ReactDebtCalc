38e6b2a1b030fba8fc9dfaf2d8d43bc7d4daa8b8
jdbanuel <jamesdb1995@gmail.com>
2020-02-18 22:01:07 -0800

    Pushing to keep changes in sync across devices

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
 import Navbar from '../src/components/Navbar.component.jsx';
+import MonthCalcForm from '../src/components/MonthCalculatorForm.component.jsx';
 
 function App() {
     return (
-        <div className="container mx-auto">
+        <div className="container mx-auto h-screen">
             <Navbar />
-            <div className="flex justify-center">
+            <div className="flex justify-center my-8 h-full">
+                <div className="flex flex-col w-2/5 h-xl max-w-3xl border border-gray-500 rounded shadow-xl">
+                    <div className="flex justify-center text-2xl bg-gray-600 text-gray-100 font-semibold">
+                        Enter your debt &amp; interest rate
+                    </div>
+
+                    <MonthCalcForm class="flex justify-center" />
+                </div>
+            </div>
+            {/* <div className="flex justify-center">
                 <div className="flex-none">Left</div>
                 <div classname="flex-none">Right</div>
-            </div>
+            </div> */}
         </div>
     );
 }
