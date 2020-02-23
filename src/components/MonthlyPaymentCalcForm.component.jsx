0a5ae46aa17f32a14bc7a073583d0367faa9b247
jdbanuel <jamesdb1995@gmail.com>
2020-02-23 00:55:19 -0800

    Fixing JSX errors

--- src/components/MonthlyPaymentCalcForm.component.jsx
+++ src/components/MonthlyPaymentCalcForm.component.jsx
@@ -22,16 +22,16 @@ class MonthlyPaymentCalcForm extends React.Component {
 
     render() {
         return (
-            <div class="w-full flex justify-center">
-                <form onSubmit={this.handleSubmit} class="w-2/3 px-8 pb-8">
-                    <div class="mb-4">
+            <div className="w-full flex justify-center">
+                <form onSubmit={this.handleSubmit} className="w-2/3 px-8 pb-8">
+                    <div className="mb-4">
                         <label
-                            class="block text-gray-700 text-sm font-bold mb-3"
-                            for="numberOfMonths"
+                            className="block text-gray-700 text-sm font-bold mb-3"
+                            htmlFor="numberOfMonths"
                         >
                             Month
                             <input
-                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="numberOfMonths"
                                 placeholder="Months to Payoff"
                                 onChange={this.handleChange}
@@ -42,7 +42,7 @@ class MonthlyPaymentCalcForm extends React.Component {
                     </div>
 
                     <button
-                        class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                         type="submit"
                         value="Submit"
                     >
