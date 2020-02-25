7e06bd55ba299ca4061ec27c18b9759c49ea8b22
jdbanuel <jamesdb1995@gmail.com>
2020-02-24 23:04:38 -0800

    Lifting state up to parent component (CardDisplay)

--- src/components/DebtAPRForm.component.jsx
+++ src/components/DebtAPRForm.component.jsx
@@ -1,25 +1,6 @@
 import React from 'react';
 
 class DebtAPRForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            debt: '',
-            interestRate: ''
-        };
-
-        this.handleDebtChange = this.handleDebtChange.bind(this);
-        this.handleInterestChange = this.handleInterestChange.bind(this);
-    }
-
-    handleDebtChange(event) {
-        this.setState({ debt: event.target.value });
-    }
-
-    handleInterestChange(event) {
-        this.setState({ interestRate: event.target.value });
-    }
-
     render() {
         return (
             <div className="w-full flex justify-center">
@@ -34,8 +15,8 @@ class DebtAPRForm extends React.Component {
                                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="debt"
                                 placeholder="Total Debt"
-                                onChange={this.handleDebtChange}
-                                value={this.state.debt}
+                                onChange={this.props.handleDebtChange}
+                                value={this.props.debt}
                                 type="text"
                             />
                         </label>
@@ -48,8 +29,8 @@ class DebtAPRForm extends React.Component {
                                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="interestRate"
                                 placeholder="Interest Rate"
-                                onChange={this.handleInterestChange}
-                                value={this.state.interestRate}
+                                onChange={this.props.handleInterestChange}
+                                value={this.props.interestRate}
                                 type="text"
                             />
                         </label>
--- src/components/MonthlyPaymentCalcForm.component.jsx
+++ src/components/MonthlyPaymentCalcForm.component.jsx
@@ -1,29 +1,13 @@
 import React from 'react';
 
 class MonthlyPaymentCalcForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            numMonths: ''
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({ numMonths: event.target.value });
-    }
-
-    handleSubmit(event) {
-        alert('Todo: This is when the calculation will be computed.');
-        event.preventDefault();
-    }
-
     render() {
         return (
             <div className="w-full flex justify-center">
-                <form onSubmit={this.handleSubmit} className="w-2/3 px-8 pb-8">
+                <form
+                    onSubmit={this.props.handleFormSubmit}
+                    className="w-2/3 px-8 pb-8"
+                >
                     <div className="mb-4">
                         <label
                             className="block text-gray-700 text-sm font-bold mb-3"
@@ -34,8 +18,8 @@ class MonthlyPaymentCalcForm extends React.Component {
                                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="numberOfMonths"
                                 placeholder="Months to Payoff"
-                                onChange={this.handleChange}
-                                value={this.state.numMonths}
+                                onChange={this.props.handleMonthsChange}
+                                value={this.props.numMonths}
                                 type="text"
                             />
                         </label>
